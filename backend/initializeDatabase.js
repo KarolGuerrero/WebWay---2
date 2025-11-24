172d0dca020ad179b825b790c98ebeb2ce9415da
KarolGuerrero <stefaniortiz2905@gmail.com>
2025-11-24 01:43:06 -0500

    conexion base de datos

--- backend/initializeDatabase.js
+++ backend/initializeDatabase.js
@@ -4,9 +4,9 @@ const bcrypt = require('bcrypt');
 // Configuración de conexión
 const client = new Client({
   connectionString: process.env.DATABASE_URL,
-  ssl: {
+  ssl: process.env.NODE_ENV === 'production' ? {
     rejectUnauthorized: false
-  }
+  } : false
 });
 
 // Tus POIs
